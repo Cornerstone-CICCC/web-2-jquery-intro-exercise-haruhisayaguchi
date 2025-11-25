cb41a96f227bda6f31d712780a29df4415b06c32
haruhisayaguchi <hal.yaguchi@gmail.com>
2025-11-24 20:41:54 -0800

    WAD202_W2D1 completed exercise

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-$(function() {
-  // YOUR CODE HERE
+$(function () {
+  // Exercise 1
+  $("#toggleButton").on("click", function () {
+    $("#myParagraph").slideToggle();
+  })
+
+  // Exercise 2
+  $("#colorButton").on("click", function () {
+    $("#colorDiv").css({ "background-color": "yellow" })
+  })
+
+  // Exercise 3
+  $("#addClassButton").on("click", function () {
+    $("#classDiv").addClass("newClass")
+  })
+  $("#removeClassButton").on("click", function () {
+    $("#classDiv").removeClass("newClass")
+  })
+
+  // Exercise 4
+  $("#fadeInButton").on("click", function () {
+    $("#fadeDiv").fadeIn();
+  })
+  $("#fadeOutButton").on("click", function () {
+    $("#fadeDiv").fadeOut();
+  })
+
+  // Exercise 5
+  $("#slideUpButton").on("click", function () {
+    $("#slideDiv").slideUp();
+  })
+  $("#slideDownButton").on("click", function () {
+    $("#slideDiv").slideDown();
+  })
+
 })
